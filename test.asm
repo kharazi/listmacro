69f85e9d4199bed0ee5b31c4a922dfd8792bd1ff
mamadica <mohamad_dolatshah@yahoo.com>
2012-11-21 19:45:17 +0330

    append and show added

--- test.asm
+++ test.asm
@@ -5,8 +5,8 @@
 
 ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD
 
-INCLuDE io.h 
-INCLuDE list.h
+INCLUDE io.h 
+INCLUDE list.h
 
 cr EQU 0dh 
 Lf EQU 0ah 
@@ -22,19 +22,11 @@ string	BYTE	20	DUP(?)
 .CODE
 _start:	
 
-	;mov eax,34
-	;push eax
-	;call malloc
-	
 	list 20
+	append	eax,7
+	append	eax,4
+	show	eax
 	
-	mov DWORD ptr [eax],2
-	dtoa	string,[eax]
-	output string
-	
-	mov DWORD ptr [eax+8],1
-	dtoa	string,[eax+8]
-	output string
 	
 		
 	INVOKE ExitProcess, 0 	;exit with return code 0
