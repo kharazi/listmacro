e1f0f244dbd8f36588d4d481777bebfeafa4cedb
mamadica <mohamad_dolatshah@yahoo.com>
2012-11-21 23:17:32 +0330

    min and show,delete,pop,top added

--- list.h
+++ list.h
@@ -14,8 +14,8 @@ Lf EQU 0ah
 
 .DATA
 append_error	BYTE	"ERROR!",cr,Lf,"len=size",0
-reshte			BYTE	11 DUP(?)
-
+reshte			BYTE	11 DUP(?),0
+reshte2			BYTE	'*',0
 .CODE
 
 
@@ -25,8 +25,7 @@ arr STRUCT
 	len 		DWORD 		?
 arr ENDS
 
-list	macro	siz
-
+list	macro	siz					;mov address to eax
 		
 		mov 	eax,siz					
 		push 	eax
@@ -45,8 +44,8 @@ list	macro	siz
 		
 ENDM		
 
-append	macro	adres,value
-
+append	macro	adres,value			
+local	continue
 		
 		pushad									;save	registers
 		
@@ -57,43 +56,119 @@ append	macro	adres,value
 		mov		edx,(arr PTR[eax]).s
 	
 
-		;cmp 	ebx,edx
-		;jl		continue						;jump if len<s 
+		cmp 	ebx,edx							;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		jl		continue						;jump if len<s 
 												;else if len>s or len=s	
-		;output 	append_error
-		;INVOKE ExitProcess, 0 					;exit with return code 0
+		output 	append_error
+		INVOKE ExitProcess, 0 					;exit with return code 0
 		
-;continue:
+continue:
 		mov		DWORD PTR[ecx+ebx*4],value		;at first len=0
 		add 	(arr PTR[eax]).len,1			;len++
 		popad									;reload registers
 
 ENDM
 		
-tes		macro	adres
+get		macro	adres,index			;move 	index	to ebx				;;;;;;;;;;;;;;;must check index
+
+		mov		eax,adres						;address of object
+		mov 	ecx,[(arr PTR[eax]).address]
+		mov		ebx,[ecx+4*index]
+
+ENDM
+
+show	macro	adres				;output	all indexes
+local forloop,end_m
 
 		pushad									;save	registers
 		mov		eax,adres						;address of object
-		mov		ebx,(arr PTR[eax]).len
+		mov		ecx,(arr PTR[eax]).address		;address of list 	;age jaye ecx o ba ebx avaz konim error mide!
+		mov		edx,(arr PTR[eax]).len
+		jz		end_m							;if len=0
+		dec  	edx								;cause	index start from 0
+		mov 	ebx,0
+forloop:
+		dtoa 	reshte,[ecx+ebx*4]
+		output	reshte
+		inc 	ebx
+		cmp		ebx,edx
+		jle		forloop
 		
-		mov 	edx,[(arr PTR[eax]).address]
+end_m:
 		popad									;reload registers
 ENDM
 
-show	macro	adres							;just for test
+pop_back macro	adres				;pop the last index
 
-		
+		 pushad									;save	registers
+		 mov		eax,adres					;address of object
+		 dec		(arr PTR[eax]).len
+		 popad									;reload registers
+ENDM
+
+top		 macro	adres				;return's the last index in ebx
+
+		 mov		eax,adres					;address of object
+		 mov		ecx,(arr PTR[eax]).address
+		 mov		edx,(arr PTR[eax]).len
+		 dec		edx
+		 mov		ebx,[ecx+4*edx]
+ENDM
+
+delete 	 macro	adres,index			 ;delete the  index
+local forloop2,end_m2
 		pushad									;save	registers
 		mov		eax,adres						;address of object
-		mov		ecx,(arr PTR[eax]).address		;address of list							;age jaye ecx o 
-																							;ba ebx avaz konim error
-		dtoa 	reshte,[ecx]																;mide!!!!!!!!!!			
-		output	reshte
-		dtoa 	reshte,[ecx+4]																
-		output	reshte
-		dtoa 	reshte,[ecx+8]																
-		output	reshte
+		mov		ecx,(arr PTR[eax]).address		;address of list 	;age jaye ecx o ba ebx avaz konim error mide!
+		mov		edx,(arr PTR[eax]).len
+		dec 	edx
+		jz		end_m2							;if len=0
+		mov		ebx,index						;cause	index start from index
+		cmp		ebx,edx
+		jge		end_m2
+		
+		dec		(arr PTR[eax]).len				;len--
+forloop2:
+		mov		edi,ebx
+		inc		edi
+		mov		edi,[ecx+4*edi]
+		mov		DWORD PTR[ecx+4*ebx],edi
+		inc		ebx
+		cmp		ebx,edx
+		jle		forloop2
+		
+end_m2:
 		popad									;reload registers
 ENDM
 
-;min		macro	  address,siz		
+
+min		macro	  adres
+local forloop3,ebxbigger,endloop3
+
+		mov		eax,adres						;address of object
+		push 	eax
+		mov		ecx,(arr PTR[eax]).address		;address of list 	;age jaye ecx o ba ebx avaz konim error mide!
+		mov		edx,(arr PTR[eax]).len
+		
+		mov	eax,[ecx]								;eax=min
+		mov	edi,0									;edi=counter
+		cmp edx,0
+		jz 	endloop3
+		
+forloop3:
+		cmp 	edi,edx
+		jz		endloop3
+		mov 	ebx,[ecx+edi*4]						;ebx=list[i]
+		cmp 	ebx,eax								;list[i]<min
+		jg		ebxbigger
+		mov		eax,ebx
+					
+ebxbigger:	
+		inc 	edi
+		jmp 	forloop3
+					
+endloop3:
+		mov		ebx,eax								;returns min in ebx
+		pop		eax									;reload	addres in eax
+ENDM
+
