b58ab53ec10d17cbf0a3146aefc5f79bd8a47208
mamadica <mohamad_dolatshah@yahoo.com>
2012-11-21 23:17:43 +0330

    min and show,delete,pop,top added

--- test.asm
+++ test.asm
@@ -17,17 +17,55 @@ Lf EQU 0ah
 
 .DATA
 
-string	BYTE	20	DUP(?)
-
+string	BYTE	20	DUP(?),0
+endl	BYTE	cr,Lf,0
 .CODE
 _start:	
 
 	list 20
-	append	eax,7
+	append	eax,1
+	append	eax,2
+	append	eax,3
 	append	eax,4
 	show	eax
-	
-	
+		output	endl
+		
+	get		eax,1
+	dtoa	string,ebx
+	output	string
+		output	endl
+		output	endl
+		
+	pop_back 	eax
+	show	eax
+		output	endl
+		output	endl
+		
+	top		eax
+	dtoa	string,ebx
+	output	string
+		output	endl
+		output	endl
+		
+	append	eax,4
+	delete	eax,1
+	show	eax
+		output	endl
+		output	endl
+		
+	delete	eax,0
+	append	eax,2
+	append	eax,1
+	append	eax,3
+	show	eax
+		output	endl
+		output	endl
+		
+	min		eax
+	dtoa	string,ebx
+	output	string
+		output	endl
+		output	endl
 		
 	INVOKE ExitProcess, 0 	;exit with return code 0
 
