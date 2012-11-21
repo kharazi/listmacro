0de4f99510a89468bcaccf115f3507f4c97ae4c7
mamadica <mohamad_dolatshah@yahoo.com>
2012-11-21 18:07:51 +0330

    constructor has been built

--- list.h
+++ list.h
@@ -1,2 +1,27 @@
 .NOLIST     ; turn off listing
 .386
+
+extern C malloc:proc
+includelib	 msvcrt.dll
+
+arr STRUCT
+	address		DWORD		?
+	s	 		DWORD 		?
+	len 		DWORD 		?
+arr ENDS
+
+list	macro	siz
+
+		
+		mov eax,siz		
+		push eax
+		call malloc
+		
+		ASSUME ebx:PTR arr
+		mov [ebx].address,eax
+		mov [ebx].s,siz
+		mov [ebx].len,0
+		
+ENDM		
+
+;min		macro	  address,siz		
