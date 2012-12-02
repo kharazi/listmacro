66c55f2861a1957a60e1c2ab292079f077c560b5
mamadica <mohamad_dolatshah@yahoo.com>
2012-12-03 01:35:44 +0330

    bubble added

--- test.asm
+++ test.asm
@@ -20,11 +20,15 @@ Lf EQU 0ah
 string	BYTE	20	DUP(?),0
 endl	BYTE	cr,Lf,0
 
-prompt_constructor	BYTE	"list with 50 byte capacity has been made by list macro",cr,Lf,0
+prompt_constructor	BYTE	"list with 20 byte capacity has been made by list macro",cr,Lf,0
+prompt_constructor2	BYTE	"another list has been built by list.h macro",cr,Lf,0
 prompt_append1		BYTE	"values 7,9,132,2,5 has been added to list",cr,Lf,0
 prompt_append2		BYTE	"values  87,21,1,9,38 has been added to list",cr,Lf,0
+prompt_append3		BYTE	"values  7,9 has been added to this list",cr,Lf,0
 prompt_show			BYTE	"list's values:",cr,Lf,0
+prompt_show2		BYTE	"second list's values:",cr,Lf,0
 prompt_quicksort	BYTE	"list has been sort by quick sort",cr,Lf,0
+prompt_bubblesort	BYTE	"list has been sort by bubble sort",cr,Lf,0
 prompt_get			BYTE	"list[1]:",cr,Lf,0
 prompt_pop_back		BYTE	"list has been pop_back",cr,Lf,0
 prompt_top			BYTE	"list's top value:",cr,Lf,0
@@ -32,11 +36,28 @@ prompt_delete		BYTE 	"list[1] has been deleted",cr,Lf,0
 prompt_min			BYTE	"list's min value:",cr,Lf,0
 prompt_max			BYTE	"list's max value:",cr,Lf,0
 prompt_len			BYTE	"list's length:",cr,Lf,0
+prompt_find			BYTE	"index of find element 9:",cr,Lf,0
 .CODE
 _start:	
-
-	list metallica,50
+	
+	list metallica2,20
 	output	prompt_constructor
+	output	endl
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+	append	metallica2,7
+	append	metallica2,9
+	output	prompt_append3
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+	output	prompt_show2
+	show	metallica2
+		output	endl
+		output	endl
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+	list metallica,50
+	output	prompt_constructor2
 	output	endl
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
@@ -92,7 +113,6 @@ _start:
 	show	metallica
 		output	endl
 		output	endl
-	
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
 	output	prompt_min
@@ -110,7 +130,17 @@ _start:
 		output	endl
 		output	endl
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	output		prompt_bubblesort
+	BubbleSort	metallica	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	
+	output	prompt_show
+	show	metallica
+		output	endl
+		output	endl
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
+		
 	append	metallica,87
 	append	metallica,21
 	append	metallica,1
@@ -122,6 +152,14 @@ _start:
 	
 	output	prompt_show
 	show	metallica
+		output	endl
+		output	endl	
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	
+	output		prompt_find
+	find		metallica,9
+	dtoa	string,ebx
+	output	string
 		output	endl
 		output	endl
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -135,11 +173,9 @@ _start:
 		output	endl
 		output	endl
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;find		metallica,4
-	;dtoa	string,eax
-	;output	string
-	;	output	endl
-	;	output	endl
+	
+	
+	
 		
 	INVOKE ExitProcess, 0 	;exit with return code 0
 
@@ -147,5 +183,6 @@ _start:
 PUBLIC _start 			;make entry point public
 
 END 				;end of source code
+
 
 
